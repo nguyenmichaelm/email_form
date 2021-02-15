1438586aa6bd4647196cf823ab5ae203423e0dfd
Michael Nguyen <nguyenmichaelm@yahoo.com>
2021-02-14 22:35:32 -0800

    add php class and nodemailer node package to see which one would meet form to email purpose

--- email.php
+++ email.php
@@ -1,7 +1,7 @@
 <?php 
 console.log("hit");
 if(isset($_POST['submit'])){
-    $to = "nguyenmichaelm@yahoo.com"; // this is your Email address
+    $to = 'nguyenmichaelm@yahoo.com'; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
